dfdb00ba016f5114f3bcf1c7433ff5b812d6d066
hyojin030 <hyojin030@sookmyung.ac.kr>
2020-11-13 18:26:34 +0900

    init

---
+++ src/test/java/com/example/myhome2/Myhome2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhome2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myhome2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
